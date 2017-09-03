e110be6034d56ed8a93ee377c830c97a6fecad26
GrugLife <gwylie1@gmail.com>
2017-09-03 13:17:00 -0700

    inital commit

---
+++ textEditor.py
@@ -0,0 +1,21 @@
+#! python 3
+# textEditor.py - my very own text editor
+
+from Tkinter import *
+
+def onclick():
+    pass
+
+root = Tk()
+text = Text(root)
+#text.insert(INSERT, "hello.....")
+#text.insert(END, "world")
+text.pack()
+
+
+#text.tag_add("here", "1.0", "1.4")
+#text.tag_add("start", "1.8", "1.13")
+#text.tag_config("here", background = "yellow", foreground="blue")
+#text.tag_config("start", background = "black", foreground="green")
+root.mainloop()
+
